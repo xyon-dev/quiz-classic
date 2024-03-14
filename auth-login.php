e211195d5218564c563e3874414bb8abdd8908db
Terence Hastings <terencehastings.707@gmail.com>
2024-03-13 22:23:11 -0700

    update files

---
+++ auth-login.php
@@ -0,0 +1,25 @@
+<?php
+include('db.php');
+$fd_password = $_POST["password"];
+
+$stmt = $conn->stmt_init();
+//
+$stmt->prepare("SELECT ID, username, password FROM `auth` WHERE username= ?");
+//
+$stmt->bind_param("s", $_POST['email']);
+//
+$stmt->execute();
+//
+$stmt->bind_result($ID, $username, $password);
+
+//
+$stmt->fetch();
+$hashedFormPassword = hash("sha256",$fd_password);
+$hashedDBPassword = hash("sha256", $password);
+
+if($hashedDBPassword != $hashedFormPassword){ 
+    echo "Invalid username or password";
+}else{
+    echo "$ID $username";
+}
+?>
--- login.php
+++ login.php
@@ -7,37 +7,42 @@
 </head>
 <body>
 <?php
+include('db.php');
 ini_set('display_errors', 1);
-$form_data = $_POST;
-extract($form_data);
+$f_email = $_POST['email'];
+$f_password =$_POST['password'];
 // echo "$email $password $staysignedin";
 
 /**
  * AUTHORIZE USER
  */
-# connect to DB
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quiz";
+$sql = "SELECT ID, username, password FROM `auth` WHERE username=\"$f_email\";";
+// $sql = "SELECT ID, username, password FROM `auth` WHERE username= ?;";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-// $sql = "SELECT * FROM `auth`;";
+// $stmt = $conn->prepare("SELECT ID, username, password FROM `auth` WHERE username= ?;");
+// $stmt->bindParam(1, $f_email);
+// $stmt->execute;
 
 
-# look up user
-#verify password
-    // hash password
-    // compare password
-# set cookie
+$dbData = mysqli_query($conn, $sql);
+if(mysqli_num_rows($dbData) < 1){ 
+    die("ERROR: try again");
+};
+while($row = mysqli_fetch_assoc($dbData)){
+    $hashedFormPassword = hash("sha256", $f_password);
+    extract($row);
+    if($hashedFormPassword != hash("sha256", $password)){
+        echo "Invalid username or password";
+    }else{
+        //2-factor
+        //return a session key
+        //cookies
+        echo "Login successful"; 
+    }
+};
 
-?>
+?><pre><?php
+// print_r($rowData->username);
+?></pre><?php #end ?>
 </body>
 </html>
